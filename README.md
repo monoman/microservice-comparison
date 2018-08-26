76e2d0b474f323882e75fe3a69251ad526b1aa09
Rafael Teixeira <monoman@gmail.com>
2018-08-26 20:29:08 -0300

    Adding C# (ASP.NET Core 2.1) implementation

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Microservice Comparison
-Build a simple microservice in multiple languages to have a solid baseline to compare languages.
+Build a simple microservice in multiple languages to have a solid baseline to compare languages. 
+**Now including C#**
 
 # What does it do?
 1. Download json data from reddit
 2. Transform it
 3. Return transformed data
 
-# Medium Post
+# Original Medium Post
 https://medium.com/@trashhalo/microservice-comparison-63ed53a47a6c
---
+++ csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Collections;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static Newtonsoft.Json.JsonConvert;
+
+namespace microservice_comparison
+{
+    public class Program
+    {
+        public static void Main(string[] args) => WebHost
+            .Start("http://localhost:3000", async context => await context.Response.WriteAsync(SerializeObject(Map(await GetData()))))
+            .WaitForShutdown();
+
+        private static async Task<object> GetData()
+            => DeserializeObject(await new HttpClient().GetStringAsync("https://www.reddit.com/r/politics/hot.json"));
+
+        private static IEnumerable Map(dynamic reddit)
+        {
+            foreach (var children in reddit.data.children)
+                yield return new { children.data.title };
+        }
+    }
+}
