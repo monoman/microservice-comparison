fe7e96f163a480d5e630b85d904b7e9f0b7c19c5
Rafael Teixeira <rteixeira@il2.io>
2023-01-10 21:57:51 -0300

    Recompiled go version with new dependency seems to be blacklisted by reddit.com (returns html with 'Blocked <IPAddress>' message), even with more simulated request headers (rust version and Chrome browser are retrieving from reddit on the same machine/IP Address)

--- go/microservice.go
+++ go/microservice.go
@@ -24,21 +24,32 @@ func main() {
 		client := http.DefaultClient
 		req, err := http.NewRequest("GET", "https://www.reddit.com/r/politics/hot.json", nil)
 		if err != nil {
+            e.Logger.Print(err)
 			return err
 		}
-		// reddit is blacklisting default golang agent
-		req.Header.Set("User-Agent", "not golang")
+		// reddit is blacklisting golang client (such workarounds are not working)
+		req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
+		req.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+        req.Header.Set("accept-encoding", "gzip, deflate, br")
+        req.Header.Set("accept-language", "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7")
+        req.Header.Set("cache-control", "no-cache")
+        req.Header.Set("dnt","1")
+        req.Header.Set("pragma", "no-cache")
 		resp, err := client.Do(req)
 		if err != nil {
+            e.Logger.Print(err)
 			return err
 		}
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
+            e.Logger.Print(err)
 			return err
 		}
+        e.Logger.Print(string(body[:]))
 		var data Reddit
 		err = json.Unmarshal(body, &data)
 		if err != nil {
+            e.Logger.Print(err)
 			return err
 		}
 		var arr []interface{}
@@ -50,5 +61,5 @@ func main() {
 		}
 		return c.JSON(http.StatusOK, arr)
 	})
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
