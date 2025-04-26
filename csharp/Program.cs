1e74a77edc57364c79efe7a7d9dd4497b7d466bb
Rafael Teixeira <monoman@gmail.com>
2025-04-25 21:01:09 -0300

    Even shorter and with more features

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,31 +1,22 @@
-var app = WebApplication.CreateBuilder(args)
-                        .ConfigureHttpJsonOptions()
-                        .Build();
-app.MapGet("/", async () => Results.Ok(await "https://www.reddit.com/r/politics/hot.json".GetTitlesAsync()));
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddSingleton<RedditClient>()
+    .ConfigureHttpJsonOptions(o => o.SerializerOptions.WriteIndented = true);
+var app = builder.Build();
+app.MapGet("/", () => Results.Redirect("/politics"));
+app.MapGet("/{sectionName}", async (string sectionName, RedditClient client) => Results.Ok(await client.GetSectionTopListAsync(sectionName)));
 app.Run("http://localhost:8080");
 
-public static class Code {
-    public record Listing(Listing.ListingData Data) {
-        public record ListingData(ListingData.Article[] Children) {
-            public record Article(Article.ArticleData Data) {
-                public record ArticleData(string Title);
-            }
-        }
+public class RedditClient : HttpClient
+{
+    public RedditClient()
+    {
+        BaseAddress = new Uri("https://www.reddit.com/");
+        DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36");
     }
-    public static WebApplicationBuilder ConfigureHttpJsonOptions(this WebApplicationBuilder builder) {
-        builder.Services.ConfigureHttpJsonOptions(o => {
-            o.SerializerOptions.WriteIndented = _options.WriteIndented;
-            o.SerializerOptions.PropertyNameCaseInsensitive = _options.PropertyNameCaseInsensitive;
-        });
-        return builder;
-    }
-    private static async Task<Stream> GetStreamAsync(this string requestUri) => 
-        await new HttpClient().GetStreamAsync(requestUri);
-    private static async Task<Listing?> GetListingAsync(this Task<Stream> getStreamAsync) =>
-        await JsonSerializer.DeserializeAsync<Listing>(await getStreamAsync, _options);
-    public static async Task<Listing.ListingData.Article.ArticleData[]> GetTitlesAsync(this string requestUri) =>
-        (await requestUri.GetStreamAsync().GetListingAsync())?.Data?.Children?.Select(x => x.Data).ToArray() ?? _empty;
-    private static readonly JsonSerializerOptions _options = new() { WriteIndented = true, PropertyNameCaseInsensitive = true };
-    private static readonly Listing.ListingData.Article.ArticleData[] _empty = Array.Empty<Listing.ListingData.Article.ArticleData>();
+    public record ArticleData(string Title);
+    public record Article(ArticleData Data);
+    public record ListingData(Article[] Children);
+    public record Listing(ListingData Data);
+    public async Task<IEnumerable<ArticleData>> GetSectionTopListAsync(string section) => (await this.GetFromJsonAsync<Listing>($"r/{section}/hot.json"))?.Data?.Children?.Select(x => x.Data) ?? [];
 }
-
