3b4fc130f1d77ceea7985b7ae15e1f07dbfab573
Rafael Teixeira <monoman@gmail.com>
2025-04-26 17:42:16 -0300

    GO:Fix headers to satisfy reddit.com && upgrade unsafe dependencies

--- go/microservice.go
+++ go/microservice.go
@@ -27,14 +27,8 @@ func main() {
             e.Logger.Print(err)
 			return err
 		}
-		// reddit is blacklisting golang client (such workarounds are not working)
-		req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
-		req.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-        req.Header.Set("accept-encoding", "gzip, deflate, br")
-        req.Header.Set("accept-language", "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7")
-        req.Header.Set("cache-control", "no-cache")
-        req.Header.Set("dnt","1")
-        req.Header.Set("pragma", "no-cache")
+		req.Header.Set("user-agent", "microservice-comparison-go")
+		req.Header.Set("accept", "application/json")
 		resp, err := client.Do(req)
 		if err != nil {
             e.Logger.Print(err)
